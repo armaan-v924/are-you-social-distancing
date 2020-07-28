8186393437fe03fda469194fdae90131ae6fe7a7
AudreyL713 <2021leea@gmail.com>
2020-07-28 14:08:10 -0700

    Display Image works
    still looks kind of ugly, may want to normalize image size

--- display_image.py
+++ display_image.py
@@ -2,6 +2,7 @@
 from facenet_models import FacenetModel
 import cv2
 import data
+import numpy as np
 def display_image(image, model):
     """Using camera.take_picture() and mf.match_face, plot image and display boxes & names around faces
 
@@ -13,24 +14,34 @@ def display_image(image, model):
     --------
     None 
     """
-    for i in image:
-        bounding_boxes, resized_crop = data.find_faces(image)
-        preds = model(resized_crop)
-        preds = np.argmax(preds, axis=1)
-        for box, img, pred in zip(bounding_boxes, resized_crop, preds):
-            if pred==0:
-                color = (0, 255, 0)
-                text = "Mask"
-            else:
-                color = (0, 0, 255)
-                text = "No Mask"
-            
-            image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), color, 2)
+    # for i in image:
+    height = image.shape[0]
+    bounding_boxes, resized_crop = data.find_faces(image)
+    bounding_boxes = np.rint(bounding_boxes).astype(np.int)
+    pass_data = resized_crop[:,np.newaxis,:,:]
 
-            if box[1] < 10:
-                img = cv2.putText(img, text, (box[0], box[1]-10), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 255, 0), 1)
-            else:
-                img = cv2.putText(img, text, (box[0], box[3]-15), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 255, 0), 1)
+    pass_data = pass_data.astype(np.float32)
+    pass_data /= 255.
+    preds = model(pass_data)
+    preds = np.argmax(preds, axis=1)
+    for box, img, pred in zip(bounding_boxes, resized_crop, preds):
+        print(pred)
+        if pred==0:
+            color = (0, 255, 0)
+            text = "Mask"
+        else:
+            color = (255, 0, 0)
+            text = "No Mask"
+        
+        image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), color, 2)
 
+        if box[1] > height - 10:
+            image = cv2.putText(image, text, (box[0], box[1]-10), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 1)
+        else:
+            image = cv2.putText(image, text, (box[0], box[3]+15), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 1)
+    
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     cv2.imshow("Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import display_image as di
 import model_setup as ms
+import cv2
 
 fi = input("Input path to image file: ")
 
-try:
-    model = ms.Model(f1=20, f2=10, d1=20, input_dim=1, num_classes=10) # TODO insert parameters
+model = ms.Model(f1=20, f2=10, d1=20, input_dim=1, num_classes=2) # TODO insert parameters
+img = cv2.imread(fi)
+img = img[:,:,::-1]
 
-    di.display_image(fi, model)
-except:
-    print("Oop")
+di.display_image(img, model)
