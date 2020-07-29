145d204b2a0e644c5cc1c10ce7c4d8d957ef3e60
AudreyL713 <2021leea@gmail.com>
2020-07-28 23:28:34 -0700

    Finished display_image for images
    2 options for displaying: show separate legend or write "mask"/"no mask" next to each box

--- display_image.py
+++ display_image.py
@@ -3,45 +3,65 @@ from facenet_models import FacenetModel
 import cv2
 import data
 import numpy as np
-def display_image(image, model):
-    """Using camera.take_picture() and mf.match_face, plot image and display boxes & names around faces
+def display_image(images, model):
+    """Uses data.find_faces and a trained model to identify faces in an image, draw
+    green boxes around the faces with masks on, and draw red boxes around the faces without
+    masks on.
+
+    Resizes original image to be of height 1000 px or width 1000 px.
+    Displays a legend next to the image with boxes drawn on the faces.
+
+    CURRENTLY DOESN'T SUPPORT VIDEOS
 
     Parameters:
     -----------
-    None
+    images: list; list of np.ndarrays from cv2.imread() for images to be displayed
 
     Returns:
     --------
     None 
     """
-    # for i in image:
-    height = image.shape[0]
-    bounding_boxes, resized_crop = data.find_faces(image)
-    bounding_boxes = np.rint(bounding_boxes).astype(np.int)
-    pass_data = resized_crop[:,np.newaxis,:,:]
+    legend = cv2.imread("legend.png")
+    cv2.imshow("Legend", legend)
 
-    pass_data = pass_data.astype(np.float32)
-    pass_data /= 255.
-    preds = model(pass_data)
-    preds = np.argmax(preds, axis=1)
-    for box, img, pred in zip(bounding_boxes, resized_crop, preds):
-        print(pred)
-        if pred==0:
-            color = (0, 255, 0)
-            text = "Mask"
+    for image in images:
+        height = image.shape[0]
+        width = image.shape[1]
+
+        if height > width:
+            sf = 1000 / height
         else:
-            color = (255, 0, 0)
-            text = "No Mask"
+            sf = 1000 / width
+
+        bounding_boxes, resized_crop = data.find_faces(image)
+        bounding_boxes = np.rint(bounding_boxes*sf).astype(np.int)
+
+        pass_data = resized_crop[:,np.newaxis,:,:]
+        pass_data = pass_data.astype(np.float32)
+        pass_data /= 255.
+
+        preds = model(pass_data)
+        preds = np.argmax(preds, axis=1)
+
+        image = cv2.resize(image, (np.rint(width*sf).astype(np.int), np.rint(height*sf).astype(np.int)))
         
-        image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), color, 2)
+        for box, img, pred in zip(bounding_boxes, resized_crop, preds):
+            if pred==0:
+                color = (0, 255, 0)
+                # text = "Mask"
+            else:
+                color = (255, 0, 0)
+                # text = "Fail"
+            
+            image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), color, 2)
 
-        if box[1] > height - 10:
-            image = cv2.putText(image, text, (box[0], box[1]-10), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 1)
-        else:
-            image = cv2.putText(image, text, (box[0], box[3]+15), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 1)
-    
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+            # if box[1] > (height*sf) - 10:
+            #     image = cv2.putText(image, text, (box[0], box[1]-10), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 2)
+            # else:
+            #     image = cv2.putText(image, text, (box[0], box[3]+15), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 1)
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+        cv2.destroyWindow("Image")
 
--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import display_image as di
 import model_setup as ms
 import cv2
+import os
 
-fi = input("Input path to image file: ")
+fi = input("Input path to folder with images: ")
 
 model = ms.Model(f1=20, f2=10, d1=20, input_dim=1, num_classes=2) # TODO insert parameters
-img = cv2.imread(fi)
-img = img[:,:,::-1]
 
-di.display_image(img, model)
+images = []
+for filename in os.listdir(fi):
+    img = cv2.imread(fi + "/" + filename)
+    img = img[:,:,::-1]
+    images.append(img)
+
+di.display_image(images, model)
