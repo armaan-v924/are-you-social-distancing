71b805bf61dc1a571bd3b620d03909514022ea11
AudreyL713 <2021leea@gmail.com>
2020-07-29 01:11:12 -0700

    Displaying enabled for videos and images

--- display_image.py
+++ display_image.py
@@ -3,28 +3,26 @@ from facenet_models import FacenetModel
 import cv2
 import data
 import numpy as np
-def display_image(images, model):
+import os
+
+def convert_image(images, model, bgr=True):
     """Uses data.find_faces and a trained model to identify faces in an image, draw
     green boxes around the faces with masks on, and draw red boxes around the faces without
     masks on.
 
     Resizes original image to be of height 1000 px or width 1000 px.
-    Displays a legend next to the image with boxes drawn on the faces.
-
-    CURRENTLY DOESN'T SUPPORT VIDEOS
 
     Parameters:
     -----------
     images: list; list of np.ndarrays from cv2.imread() for images to be displayed
-
+    model: Model, trained model that will predict mask/no mask category for each face
+    bgr: boolean, True=bgr images that need to be converted (if cv2.readimg was used)
+                  False=rgb image that does not need to be converted
     Returns:
     --------
-    None 
+    list: list of converted (boxes drawn around faces) images 
     """
-    legend = cv2.imread("legend.png")
-    cv2.imshow("Legend", legend)
-
-    for image in images:
+    for i, image in enumerate(images):
         height = image.shape[0]
         width = image.shape[1]
 
@@ -46,11 +44,15 @@ def display_image(images, model):
         image = cv2.resize(image, (np.rint(width*sf).astype(np.int), np.rint(height*sf).astype(np.int)))
         
         for box, img, pred in zip(bounding_boxes, resized_crop, preds):
+
             if pred==0:
                 color = (0, 255, 0)
                 # text = "Mask"
             else:
-                color = (255, 0, 0)
+                if bgr:
+                    color = (255, 0, 0)
+                else:
+                    color = (0, 0, 255)
                 # text = "Fail"
             
             image = cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), color, 2)
@@ -59,9 +61,92 @@ def display_image(images, model):
             #     image = cv2.putText(image, text, (box[0], box[1]-10), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 2)
             # else:
             #     image = cv2.putText(image, text, (box[0], box[3]+15), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 1)
+        if bgr:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        images[i] = image
+    return images
 
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+def show_image(images, model, bgr):
+    """Displays images with boxes drawn around faces (classified as wearing a mask
+    or not wearing a mask); also displays legend for classification
+
+    Resizes original image to be of height 1000 px or width 1000 px.
+
+    Parameters:
+    -----------
+    images: list; list of np.ndarrays from cv2.imread() for images to be displayed
+    model: Model, trained model that will predict mask/no mask category for each face
+    bgr: boolean, True=bgr images that need to be converted (if cv2.readimg was used)
+                  False=rgb image that does not need to be converted
+    Returns:
+    --------
+    None 
+    """
+    legend = cv2.imread("legend.png")
+    cv2.imshow("Legend", legend)
+
+    images = convert_image(images, model, bgr)
+    for image in images:
         cv2.imshow("Image", image)
         cv2.waitKey(0)
         cv2.destroyWindow("Image")
+    cv2.destroyAllWindows()
 
+def show_video(file_path, model):
+    """Displays video with boxes drawn around faces (classified as wearing a mask
+    or not wearing a mask); also displays legend for classification
+
+    Resizes original video window to be of height 1000 px or width 1000 px.
+
+    Parameters:
+    -----------
+    file_path: string, path to video that will be displayed
+    model: Model, trained model that will predict mask/no mask category for each face
+    Returns:
+    --------
+    None 
+    """
+    cap = cv2.VideoCapture(file_path)
+    img_array = []
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+
+        if ret == True:
+            img_array.append(frame)
+        else:
+            break
+    cap.release()
+    # print("video loaded") # DEBUGGING MESSAGES
+    # print(len(img_array))
+
+    img_array = convert_image(img_array, model, False)
+    # print("video edited") # DEBUGGING
+
+    height, width, layers = img_array[0].shape
+    size = (width, height)
+
+    out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+    
+    for i in range(len(img_array)):
+        out.write(img_array[i])
+    out.release()
+    # print("video saved") # DEBUGGING
+
+    cap = cv2.VideoCapture('project.avi')
+    legend = cv2.imread("legend.png")
+    cv2.imshow("Legend", legend)
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+
+        if ret == True:
+            cv2.imshow('Video',frame)
+            if cv2.waitKey(30) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    cv2.waitKey(0)
+    cap.release()
+    cv2.destroyAllWindows()
+    os.remove('project.avi')
--- test.py
+++ test.py
@@ -7,10 +7,7 @@ fi = input("Input path to folder with images: ")
 
 model = ms.Model(f1=20, f2=10, d1=20, input_dim=1, num_classes=2) # TODO insert parameters
 
-images = []
-for filename in os.listdir(fi):
-    img = cv2.imread(fi + "/" + filename)
-    img = img[:,:,::-1]
-    images.append(img)
-
-di.display_image(images, model)
+di.show_video(fi, model)
+# img = cv2.imread(fi)
+# img = img[:,:,::-1]
+# di.show_image([img], model, True)
